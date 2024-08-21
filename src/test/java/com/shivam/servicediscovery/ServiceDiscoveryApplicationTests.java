577e17e86b96179d3764099443ac16bdffc5942e
Shivam Sharma <shivamsharma8228@gmail.com>
2024-08-21 10:39:00 +0530

    initialized a git repo

---
+++ src/test/java/com/shivam/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivam.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
